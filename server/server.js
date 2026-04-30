dfa82809d8c9cd4921cd1834601631b1b1c0b29f
Sav mao <savannmao@gmail.com>
2026-04-30 11:42:24 +1000

    update final

--- server/server.js
+++ server/server.js
@@ -111,6 +111,6 @@ app.get('/download', (req, res) => {
     });
 });
 
-// Change line 40 in server/server.js
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
